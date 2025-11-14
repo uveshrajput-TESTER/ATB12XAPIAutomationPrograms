2ef94cc539b054ea1942628bdfcb2fd406434f7c
uveshrajput-TESTER <uveshrajpute@gmail.com>
2025-11-14 11:50:47 +0530

    POJO

---
+++ src/test/java/org/example/ex_07_Payload_management/Class/manualway/BookingDates.java
@@ -0,0 +1,21 @@
+package org.example.ex_07_Payload_management.Class.manualway;
+
+public class BookingDates {
+    private String checkin;
+    private String checkout;
+    public String getCheckin() {
+        return checkin;
+    }
+
+    public void setCheckin(String checkin) {
+        this.checkin = checkin;
+    }
+
+    public String getCheckout() {
+        return checkout;
+    }
+
+    public void setCheckout(String checkout) {
+        this.checkout = checkout;
+    }
+}
--- src/test/java/org/example/ex_07_Payload_management/Map/APITesting029_Map_Payload.java
+++ src/test/java/org/example/ex_07_Payload_management/Map/APITesting029_Map_Payload.java
@@ -1,16 +1,20 @@
 package org.example.ex_07_Payload_management.Map;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
 import io.restassured.specification.RequestSpecification;
 import org.testng.annotations.Test;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class APITesting029_Map_Payload {
     RequestSpecification r;
     ValidatableResponse vr;
     Response response;
-    String token;
-    Integer bookingId;
+
 
     @Test
     public void test_POST() {
@@ -27,5 +31,38 @@ public class APITesting029_Map_Payload {
 //                "    \"additionalneeds\" : \"Lunch\"\n" +
 //                "}";
 
+// hashmap -> key and value Pair
+//        parent Hashmap -> key and value Pair, child Hashmap.
 
-}}
+        Map<String,Object> jsonBodyUsingMap = new LinkedHashMap<>();
+        jsonBodyUsingMap.put("firstname","Pramod");
+        jsonBodyUsingMap.put("lastname","Dutta");
+        jsonBodyUsingMap.put("totalprice", 123);
+        jsonBodyUsingMap.put("depositpaid", false);
+
+        Map<String,Object> bookingDatesMap = new LinkedHashMap();
+        bookingDatesMap.put("checkin","2018-01-01");
+        bookingDatesMap.put("checkout","2019-01-01");
+
+        jsonBodyUsingMap.put("bookingdates",bookingDatesMap);
+        jsonBodyUsingMap.put("additionalneeds", "Breakfast");
+        System.out.println(jsonBodyUsingMap);
+
+        // Hashmap to -> JSON? or ClaSS TO json for this We use two Library
+        // 1. Gson by google
+        // 2. Jackson API
+        r = RestAssured.given();
+        r.baseUri("https://restful-booker.herokuapp.com/");
+        r.basePath("/booking");
+        r.contentType(ContentType.JSON);
+        r.body(jsonBodyUsingMap).log().all();
+
+        Response response = r.when().post();
+
+
+        // Get Validatable response to perform validation
+        vr = response.then().log().all();
+        vr.statusCode(200);
+
+
+    }}
